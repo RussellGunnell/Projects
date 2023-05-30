3f7d73f8a4fbbda3e7895fc72e687b957ff83e94
RussellGunnell <135051986+RussellGunnell@users.noreply.github.com>
2023-05-30 14:41:50 -0500

    Add files via upload

---
+++ LineDrawingAlgorithm/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineDrawingAlgorithm
+{
+    class Point
+    {
+        //parameters
+        int x = 0;
+        int y = 0;
+
+        //constructor method
+        public Point(int targetX, int targetY)
+        {
+            x = targetX;
+            y = targetY;
+        }//method ends
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("*");
+        }//end method
+
+        public void Move(int targetX, int targetY)//Draw is called here.
+        {
+            x = targetX;
+            y = targetY;
+            Draw();
+        }//end method
+    }
+}
